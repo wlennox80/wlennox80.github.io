99fedcac2198c313d9252b7349b1ed5dcf009885
Wendy <wlennox80@gmail.com>
2022-12-21 11:09:08 -0800

    updates to home pages

--- js/lang.js
+++ js/lang.js
@@ -43,3 +43,49 @@ function use_lang() {
 		window.location = url;
 	}
 }
+
+
+var langs = [ 'en-us', 'fr-ca', 'de' ];
+var path = location.pathname;
+var curLang = "en-us";
+var lang;
+
+$(document).ready(function() {
+	for (var i = 0; i < langs.length; i++) {
+		if (path.indexOf('/' + langs[i] + '/') != -1) {
+			curLang = langs[i];
+		}
+	}
+});
+
+
+$(document).ready(function() { 
+	// set language selector current language
+	$('select#langHome').val(curLang);
+	$('select#langHome').css('visibility','visible');
+
+	// process language change
+	$('select#langHome').change(function() {
+		lang = $(this).val();
+		localStorage.setItem('langHome', langHome);
+		use_lang(lang);
+	});
+});
+
+
+// redirect
+function use_lang() {
+	for (var i = 0; i < langs.length; i++) {
+		if (path.indexOf('/' + langs[i] + '/') != -1) {
+			curLang = langs[i];
+		}
+	}
+
+	var filename = path.substring(path.lastIndexOf('/') + 1);
+	if (path.indexOf('/' + langHome + '/') == -1) {
+		var url = window.location.toString();
+		url = url.replace(curLang, langHome);
+		url = url.substring(0, url.indexOf(langHome)) + langHome + "/index.htm";
+		window.location = url;
+	}
+}
